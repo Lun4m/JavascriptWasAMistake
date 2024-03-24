82317d32507b2a65c56fd6259662f7efa6f206ef
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-03-24 14:42:35 +0100

    Add argument parsing

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const { crawlPage } = require("./crawl");
+
+function main() {
+  const { argv, exit } = require("node:process");
+
+  if (argv.length !== 3) {
+    console.log("USAGE: npm run start <url>");
+    exit();
+  }
+  const baseURL = argv[2];
+  console.log(`Crawler initialized at ${baseURL}`);
+  crawlPage(baseURL);
+}
+
+main();
